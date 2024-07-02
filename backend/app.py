da5e125965eaf5742a8033f9ddd875c1707f9964
Chung Dinh <hgchung.dinh@gmail.com>
2024-07-02 18:09:52 +0700

    [BACKEND][FEAT]: add CORS middleware

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from models.dto.user import SignInRequest, SignUpRequest
+from models.user import UserModel
+from repositories.user import UserRepository
+from utils.password_handler import PasswordHandler
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        'http://127.0.0.1:5173'
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+repo = UserRepository()
+repo.create(SignUpRequest(name = "Chung Dinh", email = "hgchung.dinh@gmail.com", password = '123'))
+
+@app.get("/hash/{password}")
+def get_hashed(password: str):
+    return {"hash": PasswordHandler.hash(password)}
+
+@app.get("/user/{email}")
+def find_user(email: str):
+    return {"response": repo.findOne(email)}
+
+@app.post("/api/signup")
+def signup():
+    pass
