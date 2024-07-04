a99a568ca3ef47644b2cb4d5e000d32eb0f4ccbf
Chung Dinh <hgchung.dinh@gmail.com>
2024-07-04 16:33:08 +0700

    [BACKEND][CHORE]: apiCall function

---
+++ frontend/src/utils.js
@@ -0,0 +1,24 @@
+import cfg from "./config";
+
+// Got this from hah4
+export async function apiCall(
+    path = "/api",
+    method = "GET",
+    body = null,
+) {
+    try {
+        const headers = {};
+        if (body) {
+            headers["Content-Type"] = "application/json";
+        }
+        const res = await fetch(cfg.BE_URL + path, {
+            method,
+            headers,
+            credentials: "include",
+            body: body ? JSON.stringify(body) : null,
+        });
+        return await res.json();
+    } catch (err) {
+        return null;
+    }
+}
