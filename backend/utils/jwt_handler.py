f4bde7b93f854abb6068a557faadaa31297911ce
Chung Dinh <hgchung.dinh@gmail.com>
2024-07-11 14:01:38 +0700

    [BACKEND][FEAT]: add JWTHandler

---
+++ backend/utils/jwt_handler.py
@@ -0,0 +1,67 @@
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+from json import dumps, loads
+import hmac
+from hashlib import sha256
+from config import config
+
+class Base64:
+    @staticmethod
+    def encode(data: str) -> str:
+        return urlsafe_b64encode(data.encode()).decode().rstrip('=')
+    
+    @staticmethod
+    def decode(data: str) -> str:
+        enc = data + '=' * (-len(data) % 4)
+        return urlsafe_b64decode(enc.encode()).decode()
+
+class JWTHandler:
+    '''
+    Currently, there's no expiration since I haven't learned Redis
+
+    In practice, each jwt have its' own secret which is stored in Redis. Right now, I am using the same for
+    '''
+
+    @staticmethod
+    def create(payload: dict) -> str:
+        try:
+            secret = 'secret'
+            assert(secret != None)
+
+            '''
+            Expiration date of the token (Redis)
+            '''
+
+            return ".".join([
+                Base64.encode(dumps({"alg": "HS256", "typ": "JWT"})),
+                Base64.encode(dumps(payload)),
+                JWTHandler.sign(payload, secret)
+            ])
+        except Exception as e:
+            return str(e)
+    
+    @staticmethod
+    def verify(signature: str) -> bool:
+        # Notice the expiration part, too
+        try:
+            _, payload, signature = str.split('.')
+
+            '''
+            Expiration date here (Redis)
+            Might add a new UserModel's properties (uuid)
+            '''
+
+            payload = loads(Base64.decode(payload))
+            secret = 'secret'
+            assert signature == JWTHandler.sign(payload, secret)
+
+            return [payload, None]
+        except Exception as e:
+            return [None, e]
+            
+    
+    @staticmethod
+    def sign(payload: dict, secret: str) -> str:
+        payload = Base64.encode(dumps({"alg": "HS256", "typ": "JWT"})) + '.' + Base64.encode(dumps(payload))
+        return Base64.encode(hmac.new(secret.encode(), payload.encode(), sha256).hexdigest())
+    
+    
